56c3c5f055157822d1787f989d30980f58bce0fa
Jirapongse Phuriphanvichai <jirapongse.phuriphanvichai@lseg.com>
2025-08-22 12:29:22 +0700

    Add files via upload

--- MRNStoryConsumer_Method2/Program.cs
+++ MRNStoryConsumer_Method2/Program.cs
@@ -1,13 +1,15 @@
 ﻿using System;
 using System.Threading;
 using System.Collections.Generic;
-using Refinitiv.DataPlatform;
-using Refinitiv.DataPlatform.Core;
-using Refinitiv.DataPlatform.Delivery.Stream;
-using Refinitiv.DataPlatform.Content;
+using LSEG.Data;
+using LSEG.Data.Core;
+using LSEG.Data.Delivery.Stream;
+using LSEG.Data.Content;
+using LSEG.Data.Content.News;
+
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
-using Refinitiv.DataPlatform.Content.News;
+
 
 namespace MRNStoryConsumer_Method2
 {
@@ -20,10 +22,9 @@ namespace MRNStoryConsumer_Method2
             private const string WebSocketHost = "<ADS/Websocker Server IP or Hostname>:<port>";
         #endregion
 
-        #region RDPUserCredential
-            private const string RDPUser = "<RDP User/Email>";
-            private const string RDPPassword = "<RDP Password>";
-            private const string RDPAppKey = "<App Key>";
+        #region DPUserCredential
+            private const string DPClientID = "<Client ID>";
+            private const string DPClientSecret = "<Client Secret>";            
         #endregion
         private static Session.State _sessionState = Session.State.Closed;
         private static readonly int runtime=60000000;
@@ -36,42 +37,43 @@ namespace MRNStoryConsumer_Method2
             ISession session;
             if(!useRDP)
             {
-                System.Console.WriteLine("Start DeploytedPlatformSession");
-                session = CoreFactory.CreateSession(new DeployedPlatformSession.Params()
-                    .Host(WebSocketHost)
-                    .WithDacsUserName(RTDSUser)
-                    .WithDacsApplicationID(appID)
-                    .WithDacsPosition(position)
-                    .OnState((s, state, msg) =>
-                    {
-                        Console.WriteLine($"{DateTime.Now}:  {msg}. (State: {state})");
-                        _sessionState=state;
-                    })
-                    .OnEvent((s, eventCode, msg) => Console.WriteLine($"{DateTime.Now}: {msg}. (Event: {eventCode})")));
-            }else
+                System.Console.WriteLine("Start DeployedPlatformSession");
+                session = PlatformSession.Definition()
+                        .Host(WebSocketHost)
+                        .DacsUserName(RTDSUser)
+                        .DacsApplicationID(appID)
+                        .DacsPosition(position)
+                        .GetSession()
+                        .OnState((state, msg, s) =>
+                        {
+                            Console.WriteLine($"{DateTime.Now}:  {msg}. (State: {state})");
+                            _sessionState = state;
+                        })
+                        .OnEvent((eventCode, msg, s) => Console.WriteLine($"{DateTime.Now}: {msg}. (Event: {eventCode})"));
+            }
+            else
             {
-                System.Console.WriteLine("Start RDP PlatformSession");
-                session = CoreFactory.CreateSession(new PlatformSession.Params()
-                    .WithOAuthGrantType(new GrantPassword().UserName(RDPUser)
-                        .Password(RDPPassword))
-                    .AppKey(RDPAppKey)
-                    .WithTakeSignonControl(true)
-                    .OnState((s, state, msg) =>
-                    {
-                        Console.WriteLine($"{DateTime.Now}:  {msg}. (State: {state})");
-                        _sessionState = state;
-                    })
-                    .OnEvent((s, eventCode, msg) => Console.WriteLine($"{DateTime.Now}: {msg}. (Event: {eventCode})")));
+                System.Console.WriteLine("Start Data Platform Session");
+                session = PlatformSession.Definition().OAuthGrantType(
+                        new ClientCredentials().ClientID(DPClientID).ClientSecret(DPClientSecret))
+                         .GetSession()
+                         .OnEvent((eventCode, msg, s) => Console.WriteLine($"{DateTime.Now}: {msg}. (Event: {eventCode})"))
+                         .OnState((state, msg, s) =>
+                         {
+                             Console.WriteLine($"{DateTime.Now}:  {msg}. (State: {state})");
+                             _sessionState = state;
+                         });
             }
             session.Open();
             if(_sessionState==Session.State.Opened)
             {
                 System.Console.WriteLine("Session is now Opened");
                 System.Console.WriteLine("Sending MRN_STORY request");
-                using var mrnNews = MachineReadableNews.Definition().OnError((stream, err) => Console.WriteLine($"{DateTime.Now}:{err}"))
-                    .OnStatus((stream, status) => Console.WriteLine(status))
-                    .NewsDatafeed(MachineReadableNews.Datafeed.MRN_STORY)
-                    .OnNewsStory((stream, newsItem) => ProcessNewsContent(newsItem.Raw));
+                using var mrnNews = MachineReadableNews.Definition()
+                    .NewsDatafeed(MachineReadableNews.Datafeed.MRN_STORY).GetStream()
+                    .OnError((err, stream) => Console.WriteLine($"{DateTime.Now}:{err}"))
+                    .OnStatus((status, stream) => Console.WriteLine(status))
+                    .OnNewsStory((newsItem, stream) => ProcessNewsContent(newsItem.Raw));
                 {
                     mrnNews.Open();
                     Thread.Sleep(runtime);
